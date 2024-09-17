cbf85f07453e7bb3cf9c3e97f708306ec9e248ab
GharbiHaithem <gharbi.haythem1988@gmail.com>
2024-09-17 16:07:49 +0200

    abc

--- src/main/java/com/CRUD/PROJECT/config/WebConfig.java
+++ src/main/java/com/CRUD/PROJECT/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://navitrack-spring-production.up.railway.app") // Remplacez ceci par l'URL de Swagger UI
+            .allowedOrigins("http://navitrack-spring-production.up.railway.app/api") // Remplacez ceci par l'URL de Swagger UI
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
