643e8f890562a3bf9ac2200d19a2658ac423ba7e
GharbiHaithem <gharbi.haythem1988@gmail.com>
2024-09-17 15:07:02 +0200

    abc

---
+++ src/main/java/com/CRUD/PROJECT/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.CRUD.PROJECT.config; // Remplace par le package approprié
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://navitrack-spring-production.up.railway.app/swagger-ui/index.html#") // Remplace par l'URL où Swagger est hébergé
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
